38596fedcc237d27ed2c77029fb04b84bb676f64
Tien Nguyen <tientnguyen24@gmail.com>
2022-11-05 11:10:04 +0700

    create application add_product, show_product, pagination

---
+++ DentalManagement.Application/Catalog/Products/DTOs/ProductCreateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DentalManagement.Application.Catalog.Products.DTOs
+{
+    public class ProductCreateRequest
+    {
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public int ProductCategoryId { get; set; }
+    }
+}
