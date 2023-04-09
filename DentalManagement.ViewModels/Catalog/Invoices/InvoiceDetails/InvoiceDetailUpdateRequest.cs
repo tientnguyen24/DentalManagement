8fe84aacbfbae7fc1cec1927e702e8db68460c31
Tien Nguyen <tientnguyen24@gmail.com>
2023-04-09 19:54:26 +0700

    add-code-alert-20230409

---
+++ DentalManagement.ViewModels/Catalog/Invoices/InvoiceDetails/InvoiceDetailUpdateRequest.cs
@@ -0,0 +1,20 @@
+﻿using DentalManagement.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalManagement.ViewModels.Catalog.Invoices.InvoiceDetails
+{
+    public class InvoiceDetailUpdateRequest
+    {
+        public int ProductId { get; set; }
+        public decimal ItemDiscountPercent { get; set; }
+        public decimal ItemDiscountAmount { get; set; }
+        public decimal ItemAmount { get; set; }
+        public decimal Quantity { get; set; }
+        public DateTime? CompletedDate { get; set; }
+        public Status Status { get; set; }
+    }
+}
